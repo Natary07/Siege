33eaad1963d708723de9bcbbaf519d362943ecc5
Natary07 <71980983+Natary07@users.noreply.github.com>
2020-12-31 16:03:43 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var ground1,ground2;
+var block1,block2,block3,block4,block5,block6,block7,block8,block9,block10,block11,block12,block13,block14,block15,block16;
+
+function preload(){
+
+
+}
+
+function setup(){
+var canvas = createCanvas(1200,400);
+ground1 = new Ground (600,200);
+
+block1 = new Block
+
+}
+ 
+function draw(){
+ground1.display();
+
+
+
+}
